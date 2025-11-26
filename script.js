58b3b177549ab567543177fee89a8ddd7778ec4e
Divyansh-0909 <singh09divyansh@gmail.com>
2025-11-26 16:14:31 +0530

    Initial commit
    Added website Header.
    Added Hero Section.

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", () => {
+    let scrollMax = document.body.scrollHeight - window.innerHeight;
+    let scrollPercent = window.scrollY / scrollMax;  
+
+    let move = scrollPercent * (window.innerWidth); // how far to slide
+
+    document.querySelector(".main").style.transform = `translateX(${-move}px)`;
+});
